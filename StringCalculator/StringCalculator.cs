572e0f273e85b2c3de7d12b8602e91971fe8dc70
Gaurav Dogra <gauru.dogra@gmail.com>
2022-03-13 02:23:07 +0000

    completed 7 steps of String Calculator

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Week3;
+
+public class StringCalculator
+{
+    static void Main()
+    {
+        Console.WriteLine(Calculate("Test string"));
+    }
+
+    public static int Calculate(string str)
+    {
+        if (String.IsNullOrEmpty(str.Trim()))
+            return 0;
+
+        char[] delimeters = new char[2] {'\n', ','};
+
+        if (str.StartsWith("//["))
+        {
+            string fullDelimeter = str.Substring(3, str.IndexOf("]") - 3);
+            delimeters[1] = fullDelimeter[0];
+            str = str[(str.IndexOf('\n') + 1)..];
+            // assuming delimeter is a string of identical characaters
+            str = str.Replace(fullDelimeter, fullDelimeter[0].ToString());
+        }
+        else if (str.StartsWith("//"))
+        {
+            delimeters[1] = str[2];
+            str = str[(str.IndexOf('\n') + 1)..];
+        }
+
+        string[] strSplitInArray = str.Split(delimeters);
+
+        return AddElements(strSplitInArray);
+    }
+
+    private static int AddElements(string[] strSplitInArray)
+    {
+        int sum = 0;
+        SortedSet<int> negativeNumbers = new();
+
+        foreach (string s in strSplitInArray)
+        {
+            bool isSucces = int.TryParse(s, out int number);
+            if (number < 0 && isSucces)
+                negativeNumbers.Add(number);
+            else if (number <= 1000) 
+                sum += isSucces ? number : throw new ArgumentException("Input does not meet the specification");
+        }
+
+        if (negativeNumbers.Count > 0)
+            throw new ArgumentException($"negatives not allowed but found {string.Join(", ", negativeNumbers)}");
+        return sum;
+    }
+}
---
+++ StringCalculatorTest/StringCalculatorTests.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using System;
+
+namespace Week3;
+
+public class StringCalculatorTests
+{
+    [TestCase("", 0)]
+    [TestCase(" ", 0)]
+    [TestCase("1", 1)]
+    [TestCase("1,2", 3)]
+    [TestCase("1 , 2 ", 3)]
+    [TestCase("1 , 1 ", 2)]
+    [TestCase("1 , 2,3,4 ", 10)]
+    [TestCase("1 , 2," +
+        "3,4 ", 10)]
+    [TestCase("1 , 2\n 3,4 ", 10)]
+    [TestCase("//;\n1;2", 3)]
+    [TestCase("1001 , 2 ", 2)]
+    [TestCase("//[***]\n1***2***3", 6)]
+    [TestCase("//[-----]\n1-----2-----3", 6)]
+    public void ShouldReturn_CorrectTotal(string input, int expectedResult)
+    {
+        Assert.That(StringCalculator.Calculate(input), Is.EqualTo(expectedResult));
+    }
+
+    [TestCase("1, -1")]
+    public void ShouldThrowException_WhenGivenOneNegativeNumber(string input)
+    {
+        Assert.That(() => StringCalculator.Calculate(input),
+            Throws.TypeOf<ArgumentException>()
+            .With.Message.Contain("-1"));
+    }
+
+    [TestCase("-1, 11, -5, 11, 21, -11, -1")]
+    public void ShouldThrowException_WhenGivenMultipleNegativeNumbers(string input)
+    {
+        Assert.That(() => StringCalculator.Calculate(input),
+            Throws.TypeOf<ArgumentException>()
+            .With.Message.Contain("-11, -5, -1"));
+    }
+}
